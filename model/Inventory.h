b98ea96d10891a3d3a42cf3ee68f7ed3b62861e4
iStevenGuerrero <>
2021-05-25 05:38:10 +0000

    add inventory and sub inventory for potions

--- model/Inventory.h
+++ model/Inventory.h
@@ -3,25 +3,53 @@
 
 #include "Item.h"
 #include <vector>
+#include <string.h>
 using std::vector;
 
 class Inventory{
 
 private:
 
+    //Vector del inventario general (item y pociones).
+
     vector<Item> inventoryVector;
+
+    //Vector del sub-inventario de pociones.
+
+    vector<Item> inventoryPotionVector;
+
+    // Número de items del inventario general.
 
     int numberItems;
 
+    //Número de pociones en el sub-inventario.
+
+    int numberPotions;
+
 public:
 
-    int getNumberItems();
+    /* Añade items al inventario general y al inventario de pociones, esto antes revisando el item dado corresponda a una poción. Revisar los comentarios dentro de la función para más detalles.*/
 
-    Item getItemByPosition(int itemPosition);
+    void addItem(Item itemToBeAdded);
+
+    /* Elimina items al inventario general y al inventario de pociones dada una posición, esto antes revisando el item dado corresponda a una poción. Revisar los comentarios dentro de la función para más detalles.*/
+
     
     void removeItemByPosition(int itemPosition);
 
-    void addItem(Item itemToBeAdded);
+    /* Retorna un entero que corresponde al número de items en el inventario general (incluyendo las pociones)*/    
+
+    int getNumberItems();
+
+    /* Retorna un entero que corresponde al número de pociones en el sub-inventario de estas mismas*/ 
+
+    int getNumberPotions(){return this->numberPotions;};
+
+    /* Retorna un item, incluyendo pociones dada una posición dentro del inventario. Revisar los comentarios dentro de la función para más detalles*/ 
+
+    Item getItemByPosition(int itemPosition);
+
+    /* Constructor y destructor*/ 
 
     Inventory();
 
