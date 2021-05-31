88b45bcc05a1ee72d52dadb72be1dd30f6f72d8d
Thetenareone <daniel.caicedoj@gmail.com>
2021-05-31 13:15:07 -0500

    add docs to combat.h

--- model/Combat.h
+++ model/Combat.h
@@ -64,11 +64,33 @@ class Combat
         Combat();
         Combat( RenderWindow& ventana, Player& player, Enemy& enemy );
 
+        /*
+        * isOver
+        * 
+        * It checks if the combat is over
+        * 
+        * @returns enCombate : attribute that is set to false when player or enemy dies
+        */
         bool isOver( ){ return enCombate; }
+
+        /*
+        * startCombat
+        * Sets the attribute in enCombate to true
+        */
         void startCombat(){ enCombate = true; }
         //bool win(){ return win; }
 
         //Use of potions
+        /*
+        * usePotionCombat
+        * 
+        * Applies the effects of the selected potion to the player
+        * 
+        * @param effectValue : how much the attribute will increase.
+        * @param effectType : the attribute to be affected. (this is an enum)
+        * @param duration : the duration in turns the effect will affect the player
+        * 
+        */
         void usePotionCombat( int effectValue, int effectType, int duration );
 
         //Fase Prueba
@@ -85,11 +107,57 @@ class Combat
         void dibujarMensaje( int personaje );
         void dibujarMensaje( int personaje, int opcion);
 
+        /*
+        * useAttackSpell
+        * 
+        * Attacks the enemy with the selected spell
+        * 
+        * @param hechizoUsado is the spell selected by the player
+        */
         void useAttackSpell( Spell &hechizoUsado );
+
+        /*
+        * useAttackSpell
+        * 
+        * Attacks the enemy with the selected spell (in this case the special spell)
+        * 
+        * @param hechizoUsado is the spell selected by the player
+        */
         void useSpecialAttackSpell( Spell &hechizoUsado );
+
+        /*
+        * attackPlayer
+        * 
+        * Attacks the player with a random selected spell from the enemy
+        * 
+        */
         void attackPlayer();
+
+        /*
+        * checkVictory
+        * 
+        * Checks if the enemy is dead.
+        * 
+        * @return true if enemy is dead, false otherwise.
+        */
         bool checkVictory();
+
+        /*
+        * checkDefeat
+        *
+        *  Checks if the player is dead.
+        * 
+        * @return true is player is dead, false otherwise.
+        */
         bool checkDefeat();
+
+        /*
+        * truEscape
+        *
+        * The player tries to escape (depends of random numbers)
+        * 
+        * @return true if player is able to escape, false otherwise.
+        */
         bool tryEscape();
 
         void renderPlayer();
